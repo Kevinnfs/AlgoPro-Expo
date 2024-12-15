46c72f454ca1ecbec4df58eb0233dcf1110af111
Kevinnfs <kevinsiahaan1811@gmail.com>
2024-12-15 17:01:20 +0700

    fix

--- app/cara.tsx
+++ app/cara.tsx
@@ -28,13 +28,12 @@ const Cara = () => {
       {/* Main Content */}
       <ScrollView className="flex-1 bg-white">
         {/* Capaian Pembelajaran Section */}
-
         <Animated.View
           className="p-6 m-4 bg-[#F9F9F9] rounded-2xl shadow-lg"
           entering={FadeInDown.duration(500).delay(300).springify()}
         >
           <View className="flex-row items-center mb-4">
-            <Ionicons name="logo-android" size={28} color="#EF8E4A" />
+            <Ionicons name="document-text-outline" size={28} color="#EF8E4A" />
             <Text
               className="text-xl text-[#333] ml-2"
               style={{ fontFamily: "LoraBold" }}
@@ -52,6 +51,7 @@ const Cara = () => {
             menggunakan aplikasi ini dengan efisien, sehingga Anda dapat
             memaksimalkan pengalaman belajar Anda.
           </Text>
+
           {/* List Tujuan Pembelajaran */}
           <Text
             className="text-xl text-[#333] mt-3"
@@ -59,26 +59,64 @@ const Cara = () => {
           >
             Memahami Tampilan Beranda
           </Text>
-          <View className="space-y-4 px-4 mt-3">
+          <View className="space-y-4 px-4 mt-3 gap-3">
             {[
-              "Tanda Seru: Pengguna dapat melihat tentang informasi aplikasi dan pembuat aplikasi",
-              "Cari Materi: Ketika pengguna mengklik kolom cari materi, maka pengguna akan diarahkan ke halaman menu materi",
-              "Kategori:\n• Contoh Program: Berisi contoh program yang dapat dipelajari.\n• Capaian dan Tujuan Pembelajaran: Menjelaskan capaian dan tujuan yang diharapkan dari materi pembelajaran yaitu mengenai algoritma dan pemrograman pada mata pelajaran informatika kelas X.\n• Panduan Penggunaan: Panduan tentang bagaimana menggunakan aplikasi ini. ",
-              "Rekomendasi: Bagian ini menunjukkan materi yang direkomendasikan",
-              "Akses Cepat\nNavigasi di bagian bawah: Terdapat ikon navigasi seperti\n• Beranda: Untuk kembali ke tampilan utama.\n• Materi: Akses ke list materi-materi yang tersedia.\n• Quiz: Akses ke latihan-latihan soal yang bisa dikerjakan oleh siswa\n• Tes Code: fitur untuk menguji kode yang dipelajari.",
+              {
+                label: "Tanda Info",
+                icon: "information-circle-outline",
+                description:
+                  "Pengguna dapat melihat tentang informasi aplikasi seperti deskripsi aplikasi, tujuan aplikasi dibuat, dan fitur-fitur utama yang ada pada aplikasi.",
+              },
+              {
+                label: "Cari Materi",
+                icon: "search-outline",
+                description:
+                  "Ketika pengguna mengklik kolom cari materi, maka pengguna akan diarahkan ke halaman menu materi",
+              },
+              {
+                label: "Contoh Program",
+                icon: "code-working-outline",
+                description:
+                  "Berisi contoh program yang dapat dipelajari berupa bahasa pemrograman C++.",
+              },
+              {
+                label: "Capaian dan Tujuan Pembelajaran",
+                icon: "document-text-outline",
+                description:
+                  "Menjelaskan capaian dan tujuan yang diharapkan dari materi pembelajaran yaitu mengenai algoritma dan pemrograman pada mata pelajaran informatika kelas X.",
+              },
+              {
+                label: "Panduan Penggunaan",
+                icon: "help-circle-outline",
+                description:
+                  "Panduan tentang bagaimana menggunakan aplikasi ini.",
+              },
+              {
+                label: "Materi",
+                icon: "school-outline",
+                description:
+                  "Menampilkan list-list materi algoritma dan pemrograman",
+              },
+              {
+                label: "Latihan",
+                icon: "ribbon-outline",
+                description:
+                  "Akses terhadap latihan soal serta petunjuk pengerjaan.",
+              },
+              {
+                label: "Tes code",
+                icon: "code-slash-outline",
+                description:
+                  "Akses untuk mencoba menguji code bahasa C++ serta daftar tugas kelompok yang harus dikerjakan oleh siswa.",
+              },
             ].map((item, index) => (
               <View key={index} className="flex-row items-start">
+                <Ionicons name={item.icon as any} size={24} color="#EF8E4A" />
                 <Text
-                  className="text-[#EF8E4A] text-base mr-2"
-                  style={{ fontFamily: "LoraBold" }}
-                >
-                  {index + 1}.
-                </Text>
-                <Text
-                  className="text-justify text-base leading-7 text-[#555]"
+                  className="text-justify text-base leading-7 text-[#555] ml-2"
                   style={{ fontFamily: "LoraRegular" }}
                 >
-                  {item}
+                  {item.description}
                 </Text>
               </View>
             ))}
@@ -92,19 +130,20 @@ const Cara = () => {
           </Text>
           <View className="space-y-4 px-4 mt-3">
             {[
-              "Memilih Menu Materi Pembelajaran:\n• Ketuk fitur pencarian untuk mencari materi.\n• Bacalah materi dengan memilih salah satu materi yang ingin dipelajari.\n\nInteraksi dengan Materi:\n• Ketuk salah satu materi untuk melihat detail materi, dimana detail materi yang dipilih memiliki navigasi untuk membaca materi serta navigasi untuk melihat video terkait materi.\n• Beberapa materi dilengkapi dengan contoh kode yang dapat dicoba langsung pada aplikasi.",
-              "Memilih Menu Latihan:\n• Bacalah petunjuk pengerjaan sebelum memulai kuis.\n• Ketuk mulai kuis jika sudah yakin untuk mengerjakan\n\nInteraksi latihan soal:\n• Setiap soal diberi waktu 15 detik, jika sudah habis waktu otomatis ke soal selanjutnya\n• Pengguna dapat mengklik tombol selanjutnya jika sudah memilih jawaban.\n•Pengguna juga dapat memulai kuis ulang dengan mengetuk mulai setelah mendapatkan skor.",
-              "Memilih Tes Code:\n• Ketuk coba untuk mencoba menguji code bahasa C++.\n• Untuk siswa dapat mengetahui tugas apa yang harus dikerjakan dengan mengklik dropdown sesuai dengan kelompoknya.\n• Compiler code ini bersidat online yaitu membutuhkan koneksi internet.",
+              "Memilih Menu Materi Pembelajaran:\nKetuk fitur pencarian untuk mencari materi.\nBacalah materi dengan memilih salah satu materi yang ingin dipelajari.\n\nInteraksi dengan Materi:\nKetuk salah satu materi untuk melihat detail materi, dimana detail materi yang dipilih memiliki navigasi untuk membaca materi serta navigasi untuk melihat video terkait materi.",
+              "Memilih Menu Latihan:\nBacalah petunjuk pengerjaan sebelum memulai kuis.\nKetuk mulai kuis jika sudah yakin untuk mengerjakan\n\nInteraksi latihan soal:\nSetiap soal diberi waktu 15 detik, jika sudah habis waktu otomatis ke soal selanjutnya\nPengguna dapat mengklik tombol selanjutnya jika sudah memilih jawaban.\nPengguna juga dapat memulai kuis ulang dengan mengetuk mulai setelah mendapatkan skor.",
+              "Memilih Tes Code:\nKetuk coba untuk mencoba menguji code bahasa C++.\nUntuk siswa dapat mengetahui tugas apa yang harus dikerjakan dengan mengklik dropdown sesuai dengan kelompoknya.\nCompiler code ini bersidat online yaitu membutuhkan koneksi internet.",
             ].map((item, index) => (
               <View key={index} className="flex-row items-start mt-3">
                 <Text
                   className="text-[#EF8E4A] text-base mr-2"
                   style={{ fontFamily: "LoraBold" }}
                 >
-                  {index + 1}.
+                  {/* Bullet Point */}
+                  {"•"}
                 </Text>
                 <Text
-                  className="text-justify text-base leading-7 text-[#555]"
+                  className="text-justify text-base leading-7 text-[#555] ml-2"
                   style={{ fontFamily: "LoraRegular" }}
                 >
                   {item}
